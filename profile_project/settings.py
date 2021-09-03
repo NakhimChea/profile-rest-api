9d16042f5a0031aeefd21a03ff510f6cc2344499
NakhimChea <cheanakhimzzzz@gmail.com>
2021-09-03 21:08:30 +0700

    Configure ALLOW_HOST for Amazon AWS

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = '%r%*(3_@=-)e1sv2$)drvhvyh2d3v1ph+a+*c47m79e)t)-2pa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-18-223-112-55.us-east-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
